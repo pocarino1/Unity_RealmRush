cd312b5b5c0c30ae67cd2295fd4af21e9d85f69e
pocarino1 <pocarino1@gmail.com>
2023-11-02 12:40:03 +0900

    Create RealmRush Project

---
+++ Assets/Scripts/CoordinateLabeler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+    TextMeshPro label = null;
+
+    private Vector2Int coordinates = new Vector2Int();
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        DisplayCoordinates();
+    }
+
+    void Update()
+    {
+        if(!Application.isPlaying)
+        {
+            DisplayCoordinates();            
+        }
+    }
+
+    private void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt(transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x);
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z);
+
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+}
